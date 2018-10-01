763dc28c80e5f0ff2456b05155fe561888bd6459
matvii.kovtun <kovtun_m@ucu.edu.ua>
2018-10-01 14:12:39 +0300

    added point on crypto curve class

--- src/point_on_crypto_curve.py
+++ src/point_on_crypto_curve.py
@@ -1,4 +1,18 @@
 from point import Point
 
-class PointOnCurve(Point):
-    def __init__(self):
+
+class PointOnCryptoCurve(Point):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        assert PointOnCryptoCurve.point_is_on_curve(x, y)
+
+    @staticmethod
+    def point_is_on_curve(x, y):
+        epsilon = 1e-14  # due to floating point operations
+        crypto_curve = abs(y ** 2 - (x ** 3 + 7))
+        return crypto_curve < epsilon
+
+
+if __name__ == "__main__":
+    a = PointOnCryptoCurve(3, 5.830951894845301)
+    a = PointOnCryptoCurve(2, 3.872983346207417)

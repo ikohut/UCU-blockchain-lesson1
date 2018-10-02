9a789903b8c678281e9a3615e2de1eb561e2d676
retsediv <retsediv1@gmail.com>
2018-10-02 11:52:36 +0300

    Basic structure for Point

---
+++ Point.py
@@ -0,0 +1,14 @@
+class Point:
+    def __init__(self):
+        pass
+
+    def add(self, other):
+        pass
+
+
+def test_ctypto_scalar_operations():
+    pass
+
+
+if __name__ == "__main__":
+    test_ctypto_scalar_operations()

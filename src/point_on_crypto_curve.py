0c77ceec0c7241d873fa91e02e383c2a49141d49
matvii.kovtun <kovtun_m@ucu.edu.ua>
2018-10-07 16:36:36 +0300

    refactored logic in order to work with finite fields

--- src/point_on_crypto_curve.py
+++ src/point_on_crypto_curve.py
@@ -1,23 +1,32 @@
 from point import Point
+import random
+import utils
 
 
 class PointOnCryptoCurve(Point):
+    crypto_curve = {"a": 0, "b": 7, "p": 100000000003}
+
     def __init__(self, x, y):
         super().__init__(x, y)
         assert PointOnCryptoCurve.point_is_on_curve(x, y)
 
-    def add(self, other):
+    def add(self, other, mod=43):
         self._check_type(other)
 
+        l_numerator = (other.y - self.y)
+        l_denominator = (other.x - self.x)
+
         if self == other:
-            l = 3 * (self.x ** 2) / (2 * self.y)
-        else:
-            assert self.x != other.x
-            l = (other.y - self.y) / (other.x - self.x)
+            l_denominator = (2 * self.y)
+            l_numerator = 3 * (self.x ** 2)
+        elif self.x == other.x:
+            return "point at infinity"
 
-        return self._add(other, l)
+        return self._add(other, l_numerator, l_denominator, mod)
 
-    def _add(self, other, l):
+    def _add(self, other, l_numerator, l_denominator, mod):
+        l_denominator_MI = utils.multiplicative_inverse(l_denominator, mod)
+        l = (l_numerator * l_denominator_MI) % mod
         x_res = l ** 2 - self.x - other.x
         y_res = l * (self.x - x_res) - self.y
         self.x = x_res
@@ -25,10 +34,13 @@ class PointOnCryptoCurve(Point):
         return self
 
     @staticmethod
-    def point_is_on_curve(x, y, a=0, b=7):
-        epsilon = 1e-7  # due to floating point operations
-        crypto_curve = abs(y ** 2 - (x ** 3 + a * x + b))
-        return crypto_curve < epsilon
+    def point_is_on_curve(x, y):
+        a = PointOnCryptoCurve.crypto_curve["a"]
+        b = PointOnCryptoCurve.crypto_curve["b"]
+        p = PointOnCryptoCurve.crypto_curve["p"]
+        lefths = y ** 2
+        rigthhs = x ** 3 + a * x + b
+        return lefths % p == rigthhs % p
 
 
 if __name__ == "__main__":
@@ -39,14 +51,7 @@ if __name__ == "__main__":
         print("Result is on crypto curve as well: %s\n" % PointOnCryptoCurve.point_is_on_curve(*a.get_cords()))
 
 
-    a = PointOnCryptoCurve(3, 5.830951894845301)
-    b = PointOnCryptoCurve(2, 3.872983346207417)
+    a = PointOnCryptoCurve(269294, 34361.0)
+    b = PointOnCryptoCurve(54277204636, 239885.0)
 
-    c = PointOnCryptoCurve(-1.62, 1.65785162)
-    d = PointOnCryptoCurve(2.62, 4.9984725666)
-
-    e = PointOnCryptoCurve(2.62, 4.9984725666)
-    f = PointOnCryptoCurve(2.62, 4.9984725666)
     test_addition(a, b)
-    test_addition(c, d)
-    test_addition(e, f)
---
+++ src/utils.py
@@ -0,0 +1,55 @@
+import random
+
+
+def get_point_on_curve(curve):
+    a = curve["a"]
+    b = curve["b"]
+    p = curve["p"]
+
+    trial = 0
+
+    while trial < 2 * p:
+        x = random.randint(1, p - 1)
+        trial += 1
+        if trial >= (2 * p): break
+        righths = (x ** 3 + x * a + b) % p
+
+        sq_y = righths ** 0.5
+        if int(sq_y) == sq_y:
+            y = sq_y
+            return (x, y)
+
+    print("no points found")
+
+
+def multiplicative_inverse(num, mod):
+    n = num
+    m = mod
+    x, x_old = 0, 1
+    y, y_old = 1, 0
+    while mod:
+        q = num // mod
+        num, mod = mod, num % mod
+        x, x_old = x_old - q * x, x
+        y, y_old = y_old - q * y, y
+
+    if num != 1:
+        return "NO MI. However, the GCD of %d and %d is %u" % (n, m, num)
+    else:
+        return (x_old + m) % m
+
+
+def is_prime(n):
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+
+    curve = {"a": 0, "b": 7, "p": 100000000003}
+    res = get_point_on_curve(curve)
+    pp1 = (269294, 34361.0)
+    pp2 = (54277204636, 239885.0)
+    print(res)

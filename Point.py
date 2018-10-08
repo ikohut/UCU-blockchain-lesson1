0ac4bf0857c9e0760eace7e2c6e5e4d9c63bba5e
RoksolyanaLebedenko <lebedenko@ucu.edu.ua>
2018-10-08 14:35:13 +0300

    Changes

--- Point.py
+++ Point.py
@@ -1,41 +1,42 @@
 class Point:
     def __init__(self, x, y):
-        self.x = x,
+        self.x = x
         self.y = y
+        self.a = 1
 
     # a in curve y^2 = x^3 + ax + b
-    def add(self, other, a):
-        if other.x[0] != self.x[0] and other.y != self.y:
+    def add(self, other):
+        if other.x != self.x and other.y != self.y:
             # lambda λ = (y2−y1)/(x2−x1)
-            lam = (other.y - self.y) / (other.x[0] - self.x[0])
+            lam = (other.y - self.y) / (other.x - self.x)
 
             # ν = y1 − λ*x1
-            v = self.y - lam * self.x[0]
+            v = self.y - lam * self.x
 
             # x3 = λ^2 − x1 − x2
-            x3 = lam ** 2 - self.x[0] - other.x[0]
+            x3 = lam ** 2 - self.x - other.x
             # y3 = λ*x3 + v
             y3 = lam * x3 + v
 
-            self.x, self.y = x3, y3
+            self.x, self.y = x3, -y3
             return self.x, self.y
         else:
-            self.add_equal(a)
+            self.add_equal()
 
-    def add_equal(self, a):
+    def add_equal(self):
 
             # lambda λ = (3 * x1^2 + a) / 2 * y1
-            lam = (3 * (self.x[0]**2) + a) / 2 * self.y
+            lam = (3 * (self.x**2) + self.a) / 2 * self.y
 
             # ν = y1 − λ*x1
-            v = self.y - lam * self.x[0]
+            v = self.y - lam * self.x
 
             # x3 = λ^2 − 2 * x1
-            x3 = lam ** 2 - 2 * self.x[0]
+            x3 = lam ** 2 - 2 * self.x
             # y3 = λ*x3 + v
             y3 = lam * x3 + v
 
-            self.x, self.y = x3, y3
+            self.x, self.y = x3, -y3
             return self.x, self.y
 
 
@@ -43,8 +44,8 @@ def test():
     a = Point(2, 4)
     b = Point(3, 1)
 
-    c = a.add(b, 1)
-    d = b.add_equal(1)
+    c = a.add(b)
+    d = b.add_equal()
 
     print("a + b = ", c)
     print("b + b = ", d)

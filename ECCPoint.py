d4b1a56e0623cf2b72b83e8ca8a92e11abc36a6a
Yevhenii Orenchuk <orenchuk98@gmail.com>
2018-10-23 20:15:46 +0300

    added hw1

---
+++ ECCPoint.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def equation(x, a=3, b=7):
+    return x ** 3 + a * x + b
+
+
+class Point:
+    def __init__(self, x):
+        self.x = x
+        self.y = np.sqrt(equation(x))
+
+    def add(self, other):
+        second_point = Point(other)
+
+        k = (second_point.y - self.y) / (second_point.x - self.x)   # Slope
+
+        third_point_x = k**2 - second_point.x - self.x
+        third_point_y = second_point.y + k + (third_point_x - second_point.x)
+
+        self.x = third_point_x
+        self.y = third_point_y
+
+
+if __name__ == "__main__":
+    point = Point(2)
+
+    for i in range(10):
+        print("x:", point.x, "y:", point.y)
+        point.add(i)
